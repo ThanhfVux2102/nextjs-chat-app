ef0b41b2a1f5dcfa088fd49be51058ad73033135
ThanhfVux2102 <24560017@gm.uit.edu.vn>
2025-08-13 12:58:02 +0700

    style: styling position in UI

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,4 +1,5 @@
 'use client'
+
 import ProtectedRoute from '@/components/ProtectedRoute'
 import Sidebar from '../components/Sidebar'
 import ChatBox from '../components/ChatBox'
@@ -7,27 +8,41 @@ import UserPanel from '../components/UserPanel'
 export default function ChatPage() {
   return (
     <ProtectedRoute>
-    <div
-      style={{
-        display: 'flex',
-        height: '100vh',
-        fontFamily: 'sans-serif',
-        backgroundColor: '#fff',
-        color: '#000',
-      }}
-    >
-      <div style={{ flex: 2, borderRight: '1px solid #ddd' }}>
-        <Sidebar />
+      <div
+        style={{
+          display: 'flex',
+          height: '100vh',
+          fontFamily: 'sans-serif',
+          backgroundColor: '#fff',
+          color: '#000',
+        }}
+      >
+       
+        <div style={{ 
+          flex: 2, 
+          borderRight: '1px solid #ddd',
+          backgroundColor: '#fff',
+          minWidth: '280px'
+        }}>
+          <Sidebar />
+        </div>
+        <div style={{ 
+          flex: 5, 
+          display: 'flex', 
+          flexDirection: 'column', 
+          borderRight: '1px solid #ddd',
+          backgroundColor: '#f8f9fa'
+        }}>
+          <ChatBox />
+        </div>
+        <div style={{ 
+          flex: 3,
+          backgroundColor: '#fff',
+          minWidth: '300px'
+        }}>
+          <UserPanel />
+        </div>
       </div>
-
-      <div style={{ flex: 5, display: 'flex', flexDirection: 'column', borderRight: '1px solid #ddd' }}>
-        <ChatBox />
-      </div>
-
-      <div style={{ flex: 3 }}>
-        <UserPanel />
-      </div>
-    </div>
     </ProtectedRoute>
   )
 }
